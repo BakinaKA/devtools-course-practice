2a876e211bf17cb52c0abd487a14f57fbe92a791
BakinaKA <backina.xen@yandex.ru>
2022-04-19 22:05:50 +0300

    fix cppcheck

--- modules/bakina_kseniia_betcher/include/backina_kseniia_betcher.h
+++ modules/bakina_kseniia_betcher/include/backina_kseniia_betcher.h
@@ -10,9 +10,10 @@ std::vector<int> get_random_vector(int n);
 void residue_handling(int iter, const std::vector<int>& vec,
      std::vector<int>* res);
 void partial_merger(int iter_init, const std::vector<int>& vec1,
-     const std::vector<int>& vec2,std::vector<int>* res,
+     const std::vector<int>& vec2, std::vector<int>* res,
      bool comparator(int, int) = default_comparator);
 std::vector<int> EvenOddBetcher_merge(const std::vector<int>& v1,
-     const std::vector<int>& v2, bool comparator(int, int) = default_comparator);
+     const std::vector<int>& v2, 
+     bool comparator(int, int) = default_comparator);
 
 #endif  // MODULES_BAKINA_KSENIIA_BETCHER_INCLUDE_BACKINA_KSENIIA_BETCHER_H_
